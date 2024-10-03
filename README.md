bfd912763b0e018becc82e7e18d1ddea7f313464
Vitaly <vitalikd13@gmail.com>
2024-10-03 17:37:53 +0300

    init

---
+++ README.md
@@ -0,0 +1,3 @@
+# Solana + React + Vite
+
+This template provides a minimal setup to get Solan Wallet Adapter integrated with React and Vite.
---
+++ src/App.tsx
@@ -0,0 +1,34 @@
+import "@solana/wallet-adapter-react-ui/styles.css";
+import "./App.css";
+
+import { WalletAdapterNetwork } from "@solana/wallet-adapter-base";
+import { PhantomWalletAdapter } from "@solana/wallet-adapter-phantom";
+import {
+  ConnectionProvider,
+  WalletProvider,
+} from "@solana/wallet-adapter-react";
+import {
+  WalletModalProvider,
+  WalletMultiButton,
+} from "@solana/wallet-adapter-react-ui";
+import { clusterApiUrl } from "@solana/web3.js";
+import { useMemo } from "react";
+
+const App = () => {
+  const network = WalletAdapterNetwork.Devnet;
+  const endpoint = useMemo(() => clusterApiUrl(network), [network]);
+
+  const wallets = useMemo(() => [new PhantomWalletAdapter()], []);
+
+  return (
+    <ConnectionProvider endpoint={endpoint}>
+      <WalletProvider wallets={wallets} autoConnect>
+        <WalletModalProvider>
+          <WalletMultiButton />
+        </WalletModalProvider>
+      </WalletProvider>
+    </ConnectionProvider>
+  );
+};
+
+export default App;
