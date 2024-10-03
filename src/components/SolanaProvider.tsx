917d18c1908570b0ac394f764005d1039f2a829f
Vitaly <vitalikd13@gmail.com>
2024-10-03 22:33:30 +0300

    add send tx

---
+++ src/components/SolanaProvider.tsx
@@ -0,0 +1,29 @@
+import "@solana/wallet-adapter-react-ui/styles.css";
+
+import { WalletAdapterNetwork } from "@solana/wallet-adapter-base";
+import { PhantomWalletAdapter } from "@solana/wallet-adapter-phantom";
+import {
+  ConnectionProvider,
+  WalletProvider,
+} from "@solana/wallet-adapter-react";
+import { WalletModalProvider } from "@solana/wallet-adapter-react-ui";
+import { clusterApiUrl } from "@solana/web3.js";
+import { useMemo } from "react";
+
+export const SolanaProvider = ({ children }: { children: React.ReactNode }) => {
+  const network = WalletAdapterNetwork.Devnet;
+  const endpoint = useMemo(() => clusterApiUrl(network), [network]);
+
+  const wallets = useMemo(() => [new PhantomWalletAdapter()], []);
+
+  return (
+    <ConnectionProvider endpoint={endpoint}>
+      <WalletProvider wallets={wallets} autoConnect>
+        <WalletModalProvider>
+          {/*  */}
+          {children}
+        </WalletModalProvider>
+      </WalletProvider>
+    </ConnectionProvider>
+  );
+};
--- src/main.tsx
+++ src/main.tsx
@@ -2,10 +2,12 @@ import "./global.css";
 
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
+import { Toaster } from "sonner";
 import App from "./App.tsx";
 
 createRoot(document.getElementById("root")!).render(
   <StrictMode>
     <App />
+    <Toaster />
   </StrictMode>
 );
